8fc9feeb2db4d7136092b26835ede006230f91b8
JM <jmcafferata@gmail.com>
2025-11-23 00:43:37 -0300

    feat: restructure line.html and update vite.config.js to reflect new file location

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s0lt93oagmo"
+    "revision": "0.r6jal7u6da8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
